2942fef1f5d261b6988abd6808ffbe56a8177dfc
Gehnaa Pahwa <gehnaapahwa@yahoo.com>
2025-04-06 22:49:02 -0400

    add auth guard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
     { path: '**', redirectTo: 'login' },
 ];
